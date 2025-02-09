bed9520e5b0509dc8b7eefb2f29022c9802dcf7f
cstone45 <cstone45@usf.edu>
2025-02-09 04:24:50 -0500

    Task 2 - Order Detais

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,14 @@ let customer = {
 for (let key in customer) {
     console.log(`${key}: ${customer[key]}`);
 } //Logs the Properties to be displayed in the Console
+
+//Task 2: Object Methods
+let order = {
+    orderID: 12345, //Sets OrderID to 12345
+    totalAmount: 150, //Sets Total Amount to 150 Units
+    status: "Processing", //Sets Stauts as Processing
+    displayOrder: function() {
+        return `OrderID ${this.orderID} contains ${this.totalAmount} orders and is ${this.status}`;
+    } //Defines displayOrder function to display results
+};
+console.log(order.displayOrder()) //Calls function and displays results in console
