f579d2d8cc4a8483fa8dfa3fb26efe55c0a74314
cstone45 <cstone45@usf.edu>
2025-02-09 02:59:33 -0500

    Task 1 - Customer Profile

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,9 @@
 //Task 1: Object Properties
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+}; // Declared Object Properties
+for (let key in customer) {
+    console.log(`${key}: ${customer[key]}`);
+} //Logs the Properties to be displayed in the Console
